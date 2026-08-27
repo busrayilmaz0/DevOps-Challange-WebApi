7a13c3154c6223bfabde1193280c2a3e812a342a
Büşra <busra04.yilmaz@gmail.com>
2026-08-27 14:40:45 +0300

    Tum sistem konfigurasyonu ve Dockerfile duzeltildi

--- ZiraatAPI/Program.cs
+++ ZiraatAPI/Program.cs
@@ -5,9 +5,9 @@ using ZiraatApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Render / Docker port yapılandırması
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+// Render dinamik port yönetimi
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://+:{port}");
 
 builder.Services.AddCors(options =>
 {
@@ -20,8 +20,6 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
-
-// Swagger servisleri ekleniyor
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -30,17 +28,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-// Swagger arayüzü her ortamda (Production dahil) aktif edilir
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ziraat API v1");
-    c.RoutePrefix = "swagger"; // Adres: /swagger
+    c.RoutePrefix = "swagger";
 });
 
 app.UseCors();
 
-// Kök dizine (/) gelen istekleri doğrudan Swagger sayfasına yönlendiriyoruz
 app.MapGet("/", async context =>
 {
     context.Response.Redirect("/swagger");
