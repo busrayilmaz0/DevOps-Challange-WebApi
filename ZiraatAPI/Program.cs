2801f747f4d877521d5d4560a64ff3e3631cb7bc
Büşra <busra04.yilmaz@gmail.com>
2026-08-27 14:20:17 +0300

    Ana sayfa swagger yonlendirmesi eklendi

--- ZiraatAPI/Program.cs
+++ ZiraatAPI/Program.cs
@@ -28,17 +28,22 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-// Swagger arayüzünü aktifleştiriyoruz
+// Swagger arayüzü her ortamda (Production dahil) aktif edilir
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ziraat API v1");
-    c.RoutePrefix = "swagger"; // Adres: http://localhost:5000/swagger
+    c.RoutePrefix = "swagger"; // Adres: /swagger
 });
 
 app.UseCors();
 
-app.MapGet("/", () => "Hello Ziraat Team");
-app.MapControllers();
+// Kök dizine (/) gelen istekleri doğrudan Swagger sayfasına yönlendiriyoruz
+app.MapGet("/", async context =>
+{
+    context.Response.Redirect("/swagger");
+    await Task.CompletedTask;
+});
 
+app.MapControllers();
 app.Run();
