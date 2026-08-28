e551b2fd737fcd606d5a0aea17a4ac9d15bc7d73
Büşra <busra04.yilmaz@gmail.com>
2026-08-28 10:36:39 +0300

    Model propertyleri JsonPropertyName ile esitlendi

--- ZiraatAPI/Models/User.cs
+++ ZiraatAPI/Models/User.cs
@@ -1,12 +1,24 @@
-﻿namespace ZiraatApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ZiraatApi.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [JsonPropertyName("fullName")]
         public string FullName { get; set; } = string.Empty;
+
+        [JsonPropertyName("tc")]
         public string TCNo { get; set; } = string.Empty;
+
+        [JsonPropertyName("password")]
         public string Password { get; set; } = string.Empty;
+
+        [JsonPropertyName("iban")]
         public string IBAN { get; set; } = string.Empty;
+
+        [JsonPropertyName("balance")]
         public decimal Balance { get; set; }
     }
 }
