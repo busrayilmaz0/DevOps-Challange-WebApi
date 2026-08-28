e5696b0e00fdf188049e91fc069f487a073eb466
Büşra <busra04.yilmaz@gmail.com>
2026-08-28 12:06:41 +0300

    fix: map TCNo column explicitly to Postgres

--- ZiraatAPI/Models/User.cs
+++ ZiraatAPI/Models/User.cs
@@ -1,22 +1,30 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ZiraatApi.Models
 {
+    [Table("Users")] // Veritabanındaki tablo adınız
     public class User
     {
+        [Column("Id")]
         public int Id { get; set; }
 
         [JsonPropertyName("regName")]
+        [Column("FullName")] // Veritabanındaki tam sütun adı
         public string FullName { get; set; } = string.Empty;
 
         [JsonPropertyName("regTc")]
+        [Column("TCNo")] // PostgreSQL'deki büyük/küçük harf eşleşmesi
         public string TcNo { get; set; } = string.Empty;
 
         [JsonPropertyName("regPassword")]
+        [Column("Password")]
         public string Password { get; set; } = string.Empty;
 
+        [Column("IBAN")]
         public string IBAN { get; set; } = string.Empty;
 
+        [Column("Balance")]
         public decimal Balance { get; set; }
     }
 }
